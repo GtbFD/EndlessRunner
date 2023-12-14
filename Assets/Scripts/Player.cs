edd4b3f56a408b7d98892075449dc3007de58006
Gutemberg Ferreira Dantas <gutemberg.cz@gmail.com>
2023-12-14 16:26:16 -0300

    feat(movementation): adding left and right player directions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,10 @@ public class Player : MonoBehaviour
     public float _jumpHeight;
     private float _jumpVelocity;
     public float _gravity;
+    public float horizontalSpeed;
+
+    private bool isMovingLeft;
+    private bool isMovingRight;
     
     void Start()
     {
@@ -27,6 +31,20 @@ public class Player : MonoBehaviour
             {
                 _jumpVelocity = _jumpHeight;
             }
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) 
+                && transform.position.x < 5.3f && !isMovingRight)
+            {
+                isMovingRight = true;
+                StartCoroutine(RightMove());
+            }
+            
+            if (Input.GetKeyDown(KeyCode.LeftArrow)
+                && transform.position.x > -5.3f && !isMovingLeft)
+            {
+                isMovingLeft = true;
+                StartCoroutine(LeftMove());
+            }
         }
         else
         {
@@ -36,5 +54,27 @@ public class Player : MonoBehaviour
         direction.y = _jumpVelocity;
 
         _controller.Move(direction * Time.deltaTime);
+    }
+
+    IEnumerator LeftMove()
+    {
+        for (float i = 0; i < 10; i += 0.1f)
+        {
+            _controller.Move(Vector3.left * Time.deltaTime * horizontalSpeed);
+            yield return null;
+        }
+
+        isMovingLeft = false;
+    }
+
+    IEnumerator RightMove()
+    {
+        for (float i = 0; i < 10; i += 0.1f)
+        {
+            _controller.Move(Vector3.right * Time.deltaTime * horizontalSpeed);
+            yield return null;
+        }
+
+        isMovingRight = false;
     }
 }
